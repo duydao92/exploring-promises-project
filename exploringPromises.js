7c1db7503e458e07d1a2436e26efd4f5c54ba67a
Heba <hkasaby83@gmail.com>
2021-01-21 13:24:57 -0600

    phase6 is done

--- exploringPromises.js
+++ exploringPromises.js
@@ -14,9 +14,64 @@ async function num2() {
 //Phase 2
 async function waiting() {
     const value = await num2();
-    console.log('waiting', value);
+    // console.log('waiting', value);
 }
 waiting();
 
 
 //Phase 3
+
+async function waitForMyPromise() {
+    const promise = new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('done!!!');
+        }, 1000)
+    });
+
+    const result = await promise;
+    // console.log('my promise is', result);
+}
+waitForMyPromise();
+
+//Phase 4
+
+// new Promise((resolve) => {
+//     setTimeout(() => {
+//         resolve('done!');
+//     }, 1500)
+// }).then(r => console.log('then my other promise is', r));
+
+
+//Phase 5
+
+function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+async function executePromise(){
+    await wait(2000);
+    // console.log("promise executed");
+}
+executePromise()
+
+//Phase 6
+
+const tryRandomPromise = (random) => new Promise((resolve, reject) => {
+    if (random > 0.5) {
+        resolve('success!!!');
+    } else {
+        reject('random error');
+    }
+});
+// console.log(tryRandomPromise(0.3))
+
+
+for (let i=1; i<10; i++) {
+    const random = Math.random();
+    wait(2000 + random*1000)
+        .then(() => tryRandomPromise(random))
+        .then(result => console.log('random try #', random ,i, result))
+        .catch(error => console.error('random try #', random, i, error));
+}
+
+//Phase 7
